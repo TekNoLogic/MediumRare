988945db3c2e6271c3097f9785f5877d0649b21b
tekkub :bear:  <tekkub@gmail.com>
2012-10-12 06:15:18 -0600

    Break ids out into their own file, add 1-60 rares

--- MediumRare.lua
+++ MediumRare.lua
@@ -2,55 +2,19 @@
 local myname, ns = ...
 
 
-local mobIDs = {
-	[33776] = true, -- Gondria
-	[38453] = true, -- Arcturis
-
-	-- Cataclysm
-	[50062] = true, -- Aeonaxx
-
-	[50409] = true, -- Mysterious Camel Figurine
-	[50410] = true, -- Mysterious Camel Figurine
-	[50411] = true, -- Mysterious Camel Figurine
-	[50051] = true, -- Ghostcrawler
-	[50058] = true, -- Terrorpene
-	[49822] = true, -- Jadefang
-	[51403] = true, -- Madexx (Black)
-	[51404] = true, -- Madexx (Blue)
-	[51401] = true, -- Madexx (Red)
-	[51402] = true, -- Madexx (Green)
-	[50154] = true, -- Madexx (Yellow)
-	[50159] = true, -- Sambas
-	[50138] = true, -- Karoma
-
-	[51079] = true, -- Captain Foulwind
-	[51071] = true, -- Captain Florence
-	[50005] = true, -- Poseidus
-	[50052] = true, -- Burgy Blackheart
-	[49913] = true, -- Lady LaLa
-	[50050] = true, -- Shok'sharak
-	[50053] = true, -- Thartuk the Exile
-	[50057] = true, -- Blazewing
-	[50060] = true, -- Terborus
-	[50059] = true, -- Golgarok
-	[50064] = true, -- Cyrus the Black
-	[50409] = true, -- Mysterious Camel Figurine
-	[50410] = true, -- Mysterious Camel Figurine
-	[50411] = true, -- Mysterious Camel Figurine
-	[50065] = true, -- Armagedillo
-	[50085] = true, -- Overlord Sunderfury
-	[50086] = true, -- Tarvus the Vile
-}
-
-
 local myfullname = GetAddOnMetadata(myname, "Title")
 local function Print(...) print("|cFF33FF99".. myfullname.. "|r:", ...) end
 
 
-for _,achID in pairs{1312, 2257} do
+local achievements = {
+	1312, -- Bloody Rare (Outlands)
+	2257, -- Frostbitten (Northrend)
+	7439, -- Glorious! (Pandaria)
+}
+for _,achID in pairs(achievements) do
 	for i=1,GetAchievementNumCriteria(achID) do
 		local _, critType, completed, _, _, _, _, mobID = GetAchievementCriteriaInfo(achID, i)
-		if not completed and critType == 0 then mobIDs[mobID] = true end
+		if not completed and critType == 0 then ns.ids[mobID] = true end
 	end
 end
 
@@ -63,7 +27,7 @@ local function Test(id)
 	tip:ClearLines()
 	if not tip:IsOwned(WorldFrame) then tip:SetOwner(WorldFrame, "ANCHOR_NONE") end
 	tip:SetOwner(WorldFrame, "ANCHOR_NONE")
-	tip:SetHyperlink(("unit:0xF5300%05X000000"):format(id))
+	tip:SetHyperlink(("unit:0xF53%05X00000000"):format(id))
 	return tip:IsShown() and tiptext:GetText()
 end
 
@@ -75,7 +39,7 @@ f:SetScript("OnUpdate", function(self, elap)
 	e = 0
 
 	local seen
-	for mobID in pairs(mobIDs) do
+	for mobID in pairs(ns.ids) do
 		local name = Test(mobID)
 		if name then
 			if not firstscan then
@@ -88,7 +52,7 @@ f:SetScript("OnUpdate", function(self, elap)
 				seen = true
 				Print("Already seen mob:", name)
 			end
-			mobIDs[mobID] = nil
+			ns.ids[mobID] = nil
 		end
 	end
 	if seen then Print("To reset your cache log out of the game and delete the file <WoW>/Cache/WDB/"..GetLocale().."/creaturecache.wdb") end
